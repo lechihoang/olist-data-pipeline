f63dec30b444731957048315133c134b79250a37
lechihoang <lechihoang73@gmail.com>
2025-11-18 10:04:09 +0700

    update

--- dbt_olist/models/gold/fct_order_items.sql
+++ dbt_olist/models/gold/fct_order_items.sql
@@ -5,20 +5,6 @@
   )
 }}
 
-{% if is_incremental() %}
-  {%- set max_timestamp_query -%}
-    SELECT DATE_ADD(MAX(order_date), -3) FROM {{ this }}
-  {%- endset -%}
-  
-  {%- set max_loaded_timestamp_result = run_query(max_timestamp_query) -%}
-  
-  {%- if execute and max_loaded_timestamp_result.rows[0][0] is not none -%}
-    {%- set max_loaded_timestamp = max_loaded_timestamp_result.rows[0][0] -%}
-  {%- else -%}
-    {%- set max_loaded_timestamp = '1900-01-01 00:00:00' -%} 
-  {%- endif -%}
-{% endif %}
-
 WITH order_items AS (
     SELECT * FROM {{ source('olist_silver', 'order_items') }}
 ),
@@ -27,7 +13,10 @@ orders AS (
     WHERE 
         order_status IN ('delivered', 'shipped')
         {% if is_incremental() %}
-          AND order_purchase_timestamp > '{{ max_loaded_timestamp }}'
+          AND order_purchase_timestamp > (
+            SELECT COALESCE(DATE_SUB(MAX(order_date), 3), '1900-01-01')
+            FROM {{ this }}
+          )
         {% endif %}
 )
 SELECT
--- dbt_olist/models/gold/fct_order_reviews.sql
+++ dbt_olist/models/gold/fct_order_reviews.sql
@@ -5,20 +5,6 @@
   )
 }}
 
-{% if is_incremental() %}
-  {%- set max_timestamp_query -%}
-    SELECT DATE_ADD(MAX(review_date), -3) FROM {{ this }}
-  {%- endset -%}
-  
-  {%- set max_loaded_timestamp_result = run_query(max_timestamp_query) -%}
-  
-  {%- if execute and max_loaded_timestamp_result.rows[0][0] is not none -%}
-    {%- set max_loaded_timestamp = max_loaded_timestamp_result.rows[0][0] -%}
-  {%- else -%}
-    {%- set max_loaded_timestamp = '1900-01-01 00:00:00' -%} 
-  {%- endif -%}
-{% endif %}
-
 SELECT
     r.review_id,
     r.order_id,
@@ -32,5 +18,8 @@ LEFT JOIN {{ source('olist_silver', 'orders') }} AS o
     ON r.order_id = o.order_id
 
 {% if is_incremental() %}
-  WHERE r.review_creation_date > '{{ max_loaded_timestamp }}'
+WHERE r.review_creation_date > (
+    SELECT COALESCE(DATE_SUB(MAX(review_date), 3), '1900-01-01')
+    FROM {{ this }}
+)
 {% endif %}
